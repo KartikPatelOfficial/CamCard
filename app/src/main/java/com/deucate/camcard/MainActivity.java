5bf38909ad0c2b841a04642fd7c90c57261ecdb5
KartikPatelOfficial <patelkartik1910@gmail.com>
2019-04-03 15:34:41 +0530

    Add: Live preview in activity.

--- app/src/main/java/com/deucate/camcard/MainActivity.java
+++ app/src/main/java/com/deucate/camcard/MainActivity.java
@@ -1,66 +1,50 @@
 package com.deucate.camcard;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.otaliastudios.cameraview.Audio;
+import com.otaliastudios.cameraview.CameraListener;
+import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.PictureResult;
 
 public class MainActivity extends AppCompatActivity {
 
-  private static final int CAMERA_REQUEST = 1888;
-  private ImageView imageView;
-  private static final int MY_CAMERA_PERMISSION_CODE = 100;
+  private CameraView cameraView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    this.imageView = this.findViewById(R.id.imageView1);
-    Button photoButton = this.findViewById(R.id.button1);
-    photoButton.setOnClickListener(new View.OnClickListener() {
+    cameraView = findViewById(R.id.camera);
+    cameraView.setAudio(Audio.OFF);
+    cameraView.setLifecycleOwner(this);
+    cameraView.addCameraListener(new CameraListener() {
       @Override
-      public void onClick(View v) {
-        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-          requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
-        } else {
-          Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-          startActivityForResult(cameraIntent, CAMERA_REQUEST);
-        }
+      public void onPictureTaken(@NonNull PictureResult result) {
+        super.onPictureTaken(result);
+        Toast.makeText(MainActivity.this, result.getSize().toString(), Toast.LENGTH_SHORT).show();
       }
     });
   }
 
   @Override
-  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-    if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-      Bitmap photo = (Bitmap) data.getExtras().get("data");
-      imageView.setImageBitmap(photo);
-    }
+  protected void onResume() {
+    super.onResume();
+    cameraView.open();
   }
 
   @Override
-  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-      @NonNull int[] grantResults) {
-    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    if (requestCode == MY_CAMERA_PERMISSION_CODE) {
-      if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-        Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
-        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, CAMERA_REQUEST);
-      } else {
-        Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
-      }
-    }
+  protected void onPause() {
+    super.onPause();
+    cameraView.close();
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    cameraView.destroy();
   }
 }
